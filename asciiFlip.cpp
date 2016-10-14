1b146ad76ec7ab39aa4f7826a31f5c57854b28e4
Math Constant <matthias.ricker@gmail.com>
2016-10-13 21:09:59 -0400

    Add files via upload
    fully functional ascii flip program

--- asciiFlip.cpp
+++ asciiFlip.cpp
@@ -1,8 +1,6 @@
 // asciiFlip.cpp
 // MathConstant 2016
 // This program flips an ascii picture verticaly
- 
-// only works for pictures 100 lines or less
 
 // I made this file because i wanted to flip an ascci 
 // picture but couldent find a convient way to do it elseware 
@@ -13,48 +11,56 @@
 #include <stdlib.h>
 using namespace std;
 
+// function declaration 
+string CharFlip (string subject, char char1, char char2);
+
 int main ()
 {
 	
 ifstream readFile;
 ofstream writeFile;
 
-readFile.open ("bird.txt");
+readFile.open ("enterprise.txt");
 
 int lineCount = 0;
-string line;
-string contents [100];
+string lineCounter, line;
 
-// saves each line of the file to part of contents[] and finds the # of lines in the file
-while (getline(readFile, line))
-{
-	contents [lineCount] = line;	
+// finds the # of lines in the file & allows contents to be an undefined size untill the # of lines in the file are found
+while (getline(readFile, lineCounter))
+{	
 	lineCount++;
 }
 
 readFile.close();
-writeFile.open ("flipbird.txt");
+readFile.open ("enterprise.txt");
+
+string contents [lineCount];
+
+// saves each line of the file to part of contents[] 
+for (int i = 0 ;getline(readFile, line); i ++)
+{
+	contents [i] = line;	
+}
+
+readFile.close();
+writeFile.open ("flipPicture.txt");
 
 int longestline = contents[0].length();
 
-//	
+
 for (int i=1; i < lineCount ; i++) // finds the longest line in the file 
 {
-	//cout << contents[i-1] < << "    " << longestline< endl;	// prints your origanl file plus what longest line is at the time
+	//cout << contents[i-1]  << "    " << longestline << endl;	// prints your origanl file plus what longest line is at the time
 	if (contents[i].length() > longestline) 
 	{
 		longestline = contents[i].length(); 
 	} 	
 }
-//cout << contents[lineCount] < << "    " << longestline< endl;
+//cout << contents[lineCount]  << "    " << longestline << endl;
 
-// defines functions
-string ReplaceString1 (string subject, const string& search, const string& change);
-string ReplaceString2 (string subject, string referanceLine, const string& search, const string& change);
 
 // declares strings to be used for saving the fliped lines of text
 string flipContent [lineCount];
-string flipContentFinal [lineCount];
 string spaces [lineCount];
 
 // reverses each line of text and saves it to a new array
@@ -70,39 +76,39 @@ for (int i=0; i <lineCount; i++)
 	
 	flipContent [i] = string ( contents [i].rbegin(), contents [i].rend()); // flips the line 
 	
-	// finds and replaces all slashes with backslashes and vice versa (or at least it should)
-	flipContentFinal [i] = ReplaceString1 (flipContent [i], "/", "\\");
-	flipContentFinal [i] = ReplaceString2 (flipContentFinal [i], flipContent [i], "\\", "/");
-	
+	// finds and replaces all right directional charictors with their left directional counterparts and vice versa 
+	flipContent [i] = CharFlip (flipContent [i], '/', '\\');
+	flipContent [i] = CharFlip (flipContent [i], '(', ')');
+	flipContent [i] = CharFlip (flipContent [i], '<', '>');
+	flipContent [i] = CharFlip (flipContent [i], '[', ']');
+	flipContent [i] = CharFlip (flipContent [i], '{', '}');
+
 	writeFile << spaces[i] << flipContent [i] << endl; // writes the fliped line to the new file 
 }
 
 }
-// these functions currently seem to do nothing (will fix later)
-string ReplaceString1 (string subject, const string& search, const string& change) 
+
+// replaces char1 with char2 and vice versa based on the origanly entered subject
+string CharFlip (string subject, char char1, char char2)
 {
-    int pos = 0;
+ 	size_t pos = 0;
+    string referanceLine = subject;
     
-    while (subject.find(search, pos) != string::npos) 
+    while (subject.find(char1, pos) != string::npos) 
 	{
-		pos = subject.find(search, pos);
-         subject = subject.replace(pos, search.length(), change);
-        // pos += replace.length();
+		pos = subject.find(char1, pos);
+         subject = subject.replace(pos, 1, 1 , char2);
         pos++;
     }
-    return subject;
-}
-string ReplaceString2 (string subject, string referanceLine, const string& search, const string& change) 
-{
-    int pos = 0;
     
-    while (referanceLine.find(search, pos) != string::npos) 
+	pos = 0;
+	 
+	while (referanceLine.find(char2, pos) != string::npos) 
 	{
-		pos = referanceLine.find(search, pos);
-         subject = subject.replace(pos, search.length(), change);
-         //pos += replace.length();
+		pos = referanceLine.find(char2, pos);
+         subject = subject.replace(pos, 1, 1 , char1);
          pos++;
     }
-    return subject;
+    return subject;	
 }
 
